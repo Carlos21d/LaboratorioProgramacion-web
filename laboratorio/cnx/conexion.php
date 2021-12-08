2f5ac917939477503cf092bed51bcf72f9c14857
Carlos21d <78770145+Carlos21d@users.noreply.github.com>
2021-12-08 19:53:28 -0300

    LABORATORIO
    CARLOS MOLINA 
    
    27264069

---
+++ laboratorio/cnx/conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+$con = mysqli_connect("sql208.byethost14.com","b14_30479446","Rojo*123","b14_30479446_laboratorio");
+// Check connection
+if(mysqli_connect_errno()){
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}else{
+echo '';
+} 
+?>
